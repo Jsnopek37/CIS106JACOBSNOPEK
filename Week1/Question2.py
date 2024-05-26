564f453b5b68611e5c6e0e42ba23ab40fccc67c1
Jsnopek37 <sj50160@mail.harpercollege.edu>
2024-05-26 12:35:30 -0500

    Update Question2.py

--- Week1/Question2.py
+++ Week1/Question2.py
@@ -1,1 +1,9 @@
-
+def main():
+  last_name = input("Enter your last name: ")
+  hours_worked = float(input("Enter the number of hours worked: "))
+  pay_rate = float(input("Enter your pay rate: "))
+  gross_pay = hours_worked * pay_rate
+  print(f"Last Name: {last_name}")
+  print(f"Gross Pay: ${gross_pay:.2f}")
+if __name__ == "__main__":
+  main()
